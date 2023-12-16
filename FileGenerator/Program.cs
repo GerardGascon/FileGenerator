ae3a510293a5d347ac51aa3689a063ff523b8f0f
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2023-12-16 19:49:03 +0100

    Init

---
+++ FileGenerator/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using FileGenerator;
+
+class Program {
+	static void Main() {
+		//GenerateWithInputSize();
+		GenerateWithRandomSize(50);
+	}
+
+	static void GenerateWithInputSize() {
+		Console.Write("Enter file size in KB: ");
+		string input = Console.ReadLine() ?? string.Empty;
+
+		if (input == string.Empty) {
+			Console.WriteLine("Invalid input!");
+			return;
+		}
+
+		long fileSize = long.Parse(input) * 1000;
+
+		Console.Write("Enter file name: ");
+		input = Console.ReadLine() ?? string.Empty;
+
+		if (input == string.Empty) {
+			Console.WriteLine("Invalid input!");
+			return;
+		}
+
+		CreateBinaryFile(input, fileSize);
+
+		Console.WriteLine("Binary file created successfully!");
+		Console.WriteLine($"File created at: {AppDomain.CurrentDomain.BaseDirectory}{input}");
+	}
+	
+	static void GenerateWithRandomSize(int numberOfFiles) {
+		Random rnd = new();
+		for (int i = 0; i < numberOfFiles; i++) {
+			int kb = rnd.Next(0, 2_000_000);
+			long fileSize = kb * 1024;
+			
+			string fileName = $"{kb}KB";
+			CreateBinaryFile(fileName, fileSize);
+			
+			Console.WriteLine("Binary file created successfully!");
+			Console.WriteLine($"File created at: {AppDomain.CurrentDomain.BaseDirectory}{fileName}");
+		}
+	}
+
+	static void CreateBinaryFile(string filePath, long fileSize) {
+		const int bufferSize = 1024;
+		byte[] buffer = new byte[bufferSize];
+
+		using FileStream fileStream = new($"{AppDomain.CurrentDomain.BaseDirectory}{filePath}", FileMode.Create);
+		long bytesWritten = 0;
+
+		ProgressBar p = new();
+
+		while (bytesWritten < fileSize) {
+			int bytesToWrite = (int)Math.Min(bufferSize, fileSize - bytesWritten);
+			new Random().NextBytes(buffer); // Generate random data
+
+			fileStream.Write(buffer, 0, bytesToWrite);
+
+			bytesWritten += bytesToWrite;
+
+			// Print progress
+			double progress = (double)bytesWritten / fileSize * 100;
+			p.Report(progress / 100);
+		}
+
+		p.Dispose();
+	}
+}
